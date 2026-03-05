12942c7366a829c3b1f0349d9ffe2c77a83c9c1e
szhanggg <stevez9101@gmail.com>
2026-03-05 01:05:34 -0500

    Replaced blink with native

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,17 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/completion') then
+      local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      client.server_capabilities.completionProvider.triggerCharacters = chars
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
+
 vim.lsp.enable({
   "lua_ls",
   "rust-analyzer",
@@ -9,3 +23,4 @@ vim.lsp.enable({
 })
 
 vim.diagnostic.config({ virtual_text = true })
+
